dbac1cb15e50b979f925ec4f93cde0af51d5e0fa
codeXpawan <shahpawan56@gmail.com>
2023-06-01 14:29:29 +0545

    Update done

--- loaded_object.py
+++ loaded_object.py
@@ -1,12 +1,16 @@
 import numpy as np
-from OpenGL.GL import *
 import pywavefront
-from OpenGL.GL.shaders import compileProgram, compileShader
-class Object_Loader:
-    def __init__(self, path,x, y, z, factor = 1.0):
-        self.path = path
-        self.factor = factor
-        self.x = x
+from PIL import Image
+from OpenGL.GL import *
+import Arithmetic
+from Vector3 import Vector3 as v3
+from shader import Shader
+
+
+class LoadedObject:
+    def __init__(self, path: str, x: float = 0.0, y: float = 0.0, z: float = 0.0, scale: float = 1.0):
+        """Object loaded from .obj and .mtl files, ready to be drawn."""
+        self._path = path
         self._wavefront = None
         self.vaos = None
         self._vbos = None
@@ -14,27 +18,21 @@ class Object_Loader:
         self.textures = None
         self.use_texture = False
         self.lengths = []
-        self.y = y
-        self.z = z
-        self.vert_coords = []
-        self.tex_coords = []
-        self.norm_coords = []
-        self.indices = []
-        self.buffer = []
-        self.all_indices = []
-        # self.load_model(self,file = self.path)
-        
-        # self.translate(self.x,self.y,self.z)
-        # self.vertices = self.loadMesh(self.path)
-        
-        # self.vertex_count = len(self.vertices)
-        # self.vertices = np.array(self.vertices, dtype=np.float32)
-        self._load_obj(factor)
-        
-    
-    def _load_obj(self, scale_factor: float = 1.0) -> None:
+        # Set position and model
+        self.pos = Arithmetic.create_from_translation(v3([x, y, z]))
+        self.model = self.pos
+        self._scale = scale
+        self._scale_matrix: Arithmetic = Arithmetic.create_from_scale(v3([self._scale] * 3))
+        # Load wavefront
+        self._load_obj()
+
+    def set_pos(self, pos: v3):
+        self.pos = Arithmetic.create_from_translation(pos)
+        self.model = self.pos
+
+    def _load_obj(self) -> None:
         """Loads wavefront obj and materials. Stores vertex data into VAOs and VBOs."""
-        self._wavefront = pywavefront.Wavefront(self.path, collect_faces=True, create_materials=True)
+        self._wavefront = pywavefront.Wavefront(self._path, collect_faces=True, create_materials=True)
         
         # Generate buffers
         materials_count = len(self._wavefront.materials)
@@ -52,7 +50,7 @@ class Object_Loader:
         ind = 0
         for material in self._wavefront.materials.values():
             vertex_size = material.vertex_size
-            scene_vertices = np.array(material.vertices, dtype=np.float32) * scale_factor
+            scene_vertices = np.array(material.vertices, dtype=np.float32)
             print(scene_vertices)
             # Store length and materials for drawing
             self.lengths.append(len(scene_vertices))
@@ -61,7 +59,7 @@ class Object_Loader:
             if material.texture is not None:
                 self._load_texture(material.texture.path, self.textures[ind])
                 self.use_texture = True
-            
+
             # Bind VAO
             glBindVertexArray(self.vaos[ind])
             # Fill VBO
@@ -75,7 +73,7 @@ class Object_Loader:
                 "N3F": (3, 3),  # Normal (3 floats): ind=3
                 "V3F": (0, 3),  # Position (3 floats): ind=0
             }
-    
+
             cur_off = 0  # current start offset
             for attr in material.vertex_format.split("_"):
                 if attr not in attr_format:
@@ -109,62 +107,26 @@ class Object_Loader:
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
         # Load image
-        # image = Image.open(path)
-        # image = image.transpose(Image.FLIP_TOP_BOTTOM)
-        # img_data = image.convert("RGBA").tobytes()
-        # glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        image = Image.open(path)
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)
+        img_data = image.convert("RGBA").tobytes()
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
 
-    def drew(self) -> None:
+    def draw(self, shader: Shader, model=None) -> None:
         """Draws loaded object onto GL buffer with selected shader."""
         # shader.use_program()  # Not really sure if that's how you should do it
-        vertex_shader = '''
-        #version 330
-
-        layout (location = 0) in vec3 position;
-        layout (location = 1) in vec2 texcoord;
-        layout (location = 2) in vec3 normal;
-
-        out vec2 frag_texcoord;
-        out vec3 frag_normal;
-
-        void main()
-        {
-            gl_Position = vec4(position, 1.0);
-            frag_texcoord = texcoord;
-            frag_normal = normal;
-        }
-        '''
-            
-        fragment_shader = '''
-        #version 330
-
-        in vec2 frag_texcoord;
-        in vec3 frag_normal;
-
-        out vec4 out_color;
-
-        void main()
-        {
-        vec3 light_dir = normalize(vec3(1.0, 1.0, 1.0));
-        vec3 normal = normalize(frag_normal);
-        float diffuse = max(dot(normal, light_dir), 0.0);
-        out_color = vec4(vec3(1.0) * diffuse, 1.0);
-        }
-        '''
         for vao, tex, length, mat in zip(self.vaos, self.textures, self.lengths, self.materials):
             glBindVertexArray(vao) #Bind VAO
             glBindTexture(GL_TEXTURE_2D, tex)
-            # if model is not None:
-            #     shader.set_model(model)
-            # else:
-            #     shader.set_model(Arithmetic.multiply(self._scale_matrix, self.model))
-            # shader.set_v3("material.ambient", mat.ambient)
-            # shader.set_v3("material.diffuse", mat.diffuse)
-            # shader.set_v3("material.specular", mat.specular)
-            # shader.set_float("material.shininess", mat.shininess)
-            
-            # shader_program = compileProgram(compileShader(vertex_shader, GL_VERTEX_SHADER),
-            # compileShader(fragment_shader, GL_FRAGMENT_SHADER))
-            # glUseProgram(shader_program)
+            if model is not None:
+                shader.set_model(model)
+            else:
+                shader.set_model(Arithmetic.multiply(self._scale_matrix, self.model))
+            shader.set_v3("material.ambient", mat.ambient)
+            shader.set_v3("material.diffuse", mat.diffuse)
+            shader.set_v3("material.specular", mat.specular)
+            shader.set_float("material.shininess", mat.shininess)
             glDrawArrays(GL_TRIANGLES, 0, length)
 
+
+#https://www.youtube.com/watch?v=hYZNN0MTLuc&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=3
